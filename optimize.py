b4af8799f7f60041b523d22825eacdd02618a227
van_mit <ivan.mitskovets@gmail.com>
2015-10-12 21:33:27 +0300

    mnk

--- optimize.py
+++ optimize.py
@@ -4,9 +4,11 @@
 import numpy as np
 from math import cos
 from scipy.optimize import minimize
-def myf(x):
-    return (x[0]**2-2*x[1])**2+(x[1]-cos(x[0]))**2
+def myfunc1(x):
+    return cos(x)*x
+def myfunc2(x):
+    return x*x*x/(5-x)
 
-x0 = np.array([0,4])
-res = minimize(myf, x0, method='powell', options={'xtol': 1e-8, 'disp':True})
+x0 = np.array([0, 4])
+res = minimize(myfunc1, x0, method='powell', options={'xtol': 1e-8, 'disp': True})
 dorez = res.x
