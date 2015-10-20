7d7ff73840056291e8b719b8d9b1383517db7ed0
van_mit <ivan.mitskovets@gmail.com>
2015-10-20 13:00:34 +0300

    approximate _ method

--- approximate_method.py
+++ approximate_method.py
@@ -6,7 +6,7 @@ from scipy.optimize import minimize
 
 def func1(u):
     return ((u[0]**4 + np.sin(u[0]) - u[1])**2 + (u[0] - u[0]**2 - u[1]**2)**2)
-def jakobi(x1, x2):
+def myGradient(x1, x2):
     result = np.array([[4*x1**3 + np.cos(x1), -1.0], [1.0 - 2*x1, - 2.0*x2]])
     return result
 def func2(x1, x2):
@@ -14,7 +14,7 @@ def func2(x1, x2):
     return u
 
 u0 = np.array([0.8, 0.8])
-accuracy = 0.00000001
+accuracy = 0.0000001
 
 time_00 = datetime.now().microsecond
 lib_solution = minimize(func1, u0, method = 'Nelder-Mead', options = {'xtol' : 1e-8, 'disp' : True})
@@ -24,7 +24,7 @@ time_10 = datetime.now().microsecond
 n = 0
 un = u0
 while np.linalg.norm(func2(un[0], un[1])) > accuracy:
-    un -= np.dot(np.linalg.inv(jakobi(un[0], un[1])), func2(un[0], un[1]))
+    un -= np.dot(np.linalg.inv(myGradient(un[0], un[1])), func2(un[0], un[1]))
     n += 1
 time_11 = datetime.now().microsecond
 
